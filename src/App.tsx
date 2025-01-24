acf4bbf0f2783ecb2b9e098ea06885bfce909d56
Noah Krossa <noahkrossa@gmail.com>
2025-01-24 14:00:22 +0100

    Refactor text-field styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,25 @@
-import { useContext } from "react"
+import { useContext, useState } from "react"
 import { ThemeContext } from "./utils/Theme/Context"
 import { ThemeToggle } from "./utils/Theme/ThemeToggle"
+import TextField from './common/TextField'
+import { TbHomeFilled } from "react-icons/tb"
 
 function App() {
   const {theme} = useContext(ThemeContext)
+  const [value, setValue] = useState('')
   return (
-    <div className={`${theme}-mode`} id="app">
-      <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis, placeat?</h1>
+    <div className={`flx flx-col al-cntr jf-cntr p-3 gap-5 ${theme}-mode`} id="app">
+      <TextField  
+        value={value} 
+        onChange={setValue} 
+        Icon={TbHomeFilled} 
+        secret
+      />
+      <p>{value}</p>
       <ThemeToggle />
+      <button className="button">
+        Button
+      </button>
     </div>
   )
 }
--- src/common/TextField.tsx
+++ src/common/TextField.tsx
@@ -1,22 +1,39 @@
+import { useState } from 'react'
 import type {IconType} from 'react-icons'
+import { TbEye, TbEyeClosed } from 'react-icons/tb'
 
 type TextFieldProps = {
   value:string
   Icon?: IconType,
   onChange:(value:string) => void
+  secret?: boolean
 }
 
-function TextField({Icon, value, onChange}:TextFieldProps) {
- 
+
+function TextField({Icon, value, onChange, secret}:TextFieldProps) {
+  const [type, setType] = useState(() => secret ? 'password' : 'text')
+  const toggleType = () => {
+    setType(
+      type === 'password' ? 'text' : 'password'
+    )
+  }
+
   return(
     <div className="text-field">
-      {Icon && <Icon />}
+      { Icon && <span className='icon'> <Icon /></span> }
       <input 
         onChange={ (e) => { onChange(e.target.value) } } 
         value={ value } 
         className="text-field__input"  
-        type="text"
+        type={type}
       />
+      {secret && 
+        <span className='icon' onClick={toggleType}>
+          {
+            type === 'password' ? <TbEyeClosed /> : <TbEye />
+          }
+        </span>
+      }
     </div>
   )
 }
